60edcabdcf4ee535ba453f499c672d64867a86d2
Md. Yasmi Tohabar Evon (2016831021) <silverevon10010@gmail.com>
2020-07-21 02:58:37 +0600

    Running Median And Magician and Chocolates easy problem solve

---
+++ Problems and Solutions/Heap and Priority Queue/Magician and Chocolates/magician_and_chocolates_easy.py
@@ -0,0 +1,18 @@
+def max_index(numbers):
+    return numbers.index(max(numbers))
+
+
+def magician_and_chocolates(boxes, taken_time):
+    chocolates = 0
+    while taken_time != 0:
+        large_box = max_index(boxes)
+        chocolates = chocolates + boxes[large_box]
+        boxes[large_box] = float(boxes[large_box]/2)
+        taken_time = taken_time - 1
+    return chocolates
+
+
+if __name__ == "__main__":
+    ans = magician_and_chocolates([6, 5], 3)
+    print("Ans :", int(ans))
+
